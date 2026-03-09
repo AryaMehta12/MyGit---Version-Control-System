6e023d441c24d4d4edd58ecda500fd18d5e491fd
AryaMehta12 <arya.mehta12@gmail.com>
2026-03-09 21:16:38 +0530

    Understanding parsers and syntax etc

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="MyGit - My git clone"
+    )
+    subparsers = parser.add_subparsers(
+        dest = "command",
+        help = "Available commands"
+    )
 
+    # initalizing init command
+    init_parser = subparsers.add_parser("init", help = "Initalize the repo")
+    args = parser.parse_args()
+    print(args)
 main()
