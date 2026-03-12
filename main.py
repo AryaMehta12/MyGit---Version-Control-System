d1ed793c8597fabd0ad9d9a500e12774fc81ee60
AryaMehta12 <arya.mehta12@gmail.com>
2026-03-12 17:14:21 +0530

    Added blob class, made changes to add content hash and edit index dictionary
    to add obj_dir function next

--- main.py
+++ main.py
@@ -31,8 +31,11 @@ class GitObject:
         obj_type, _  = header.split(" ")
         return cls(obj_type, content)
     
-class Blob(GitObject):
-    pass
+class Blob(GitObject): #Blob -> binary large object (stores file content)
+    def __init__(self,content):
+        super().__init__('blob',content)
+    def get_content(self)->bytes:
+        return self.content
 
 # repository class
 class Repo:
@@ -65,10 +68,35 @@ class Repo:
 
         #HEAD pointer
         self.head_file.write_text("ref: refs/heads/COOLEST\n")
-        self.index_file.write_text(json.dumps({},indent = 2))
+        self.save_index({})
         print(f"Mygit Repository Initialized, in {self.mygit_dir}")
         return True
     
+    def store_object(self,obj:GitObject):
+        obj_hash = obj.hash()
+        obj_dir = self.objects_dir / obj_hash[:2]
+        obj_file = obj_dir/ obj_hash[2:]
+
+        if not obj_file.exists():
+            obj_dir.mkdir(exist_ok = True) #do nothing if it already exists(as multiple hash files can be under the same dir)
+            obj_file.write_bytes(obj.serialize())
+
+        return obj_hash
+    
+    def load_index(self) -> dict[str,str]:
+        if not self.index_file.exists(): ##incase user deleted the file on accident
+            return {} 
+        try:
+            return json.loads(self.index_file.read_text())
+        except:
+            return {}
+    
+
+    def save_index(self, index):
+        self.index_file.write_text(json.dumps(index,indent = 2))
+    
+    def add_dir(self):
+        pass
     # 4 types of objects -> BLOB, COMMIT, TREES, TAGS
     def add_file(self,path: str):
         full_path = self.path/path
@@ -78,10 +106,18 @@ class Repo:
         # Read the file content
         content = full_path.read_bytes()
         # Create BLOB (binary large obj)
+        blob = Blob(content)
         # store the blob obj in .mygit/objects
+        blob_hash = self.store_object(blob)
         #update index to include the files
-        pass
+        index = self.load_index()
+        index[path] = blob_hash
+        self.save_index(index)
 
+        print(f"Added {path}")
+
+    def add_dir(self):
+        pass
     def add_path(self,path:str) -> None:
         full_path = self.path/path 
         if not full_path.exists():
@@ -121,7 +157,7 @@ def main():
                 return 
             
         elif args.command == "add":
-            if not repo.mygit_dir.exist():
+            if not repo.mygit_dir.exists():
                 print("MyGit Uninitialized!")
                 return
             for path in args.paths:
